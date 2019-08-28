8bcb2499d2158cc430e2b00ff409ef057a3e121b
Guilherme Faura <36975422+GuilhermeIsNotUnix@users.noreply.github.com>
2019-08-28 01:08:58 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const ip = process.argv[2];
 if(/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ip)) {
     ipInfo(ip , (err, cLoc) => {
         console.log(err || cLoc);
-        ipInfo("8.8.8.8/org", (err, cLoc) => {
-            console.log(err || cLoc);
-        });
     });
 } else {
     console.log("prism: IP faltando ou invalido...");
